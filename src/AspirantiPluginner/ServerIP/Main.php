a6a0ad196e058c7b95f09e4288f41011b3cda673
TheNewTeo <coseacaso50@gmail.com>
2017-01-25 21:41:01 +0100

    ip e porta

--- src/AspirantiPluginner/ServerIP/Main.php
+++ src/AspirantiPluginner/ServerIP/Main.php
@@ -16,6 +16,16 @@ namespace AspirantiPluginner\ServerIP;
 use pocketmine\plugin\PluginBase;
 
 class Main extends PluginBase {
-    //test
-    //test teo
+
+    public $ip;
+
+    public function onEnable()
+    {
+        $ip = file_get_contents("http://ip-api.com/line/?fields=query");
+        $porta = $this->getServer()->getPort();
+
+        $this->getServer()->getLogger()->info("L'ip del server è: " . $ip);
+        $this->getServer()->getLogger()->info("La porta del server è: " . $porta);
+
+    }
 }
