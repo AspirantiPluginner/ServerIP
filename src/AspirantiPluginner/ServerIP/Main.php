bbbb8914ffb645ccbce81518c407821389462bd5
Nextor208 <vorobez208@gmail.com>
2017-01-25 20:57:32 +0100

    Update Main.php

--- src/AspirantiPluginner/ServerIP/Main.php
+++ src/AspirantiPluginner/ServerIP/Main.php
@@ -18,6 +18,8 @@ use pocketmine\plugin\PluginBase;
 class Main extends PluginBase {
 
     public function onEnable() {
+        $this->getServer()->getLogger(OnEnable $ip = file_get_contents("http://ip-api.com/line/?fields=query"));
+        
         // :D
  }
 }
