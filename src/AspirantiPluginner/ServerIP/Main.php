05c20fe83e37ce99d6314f505e797e2d863e3422
GabboMCPE <zanatta629@gmail.com>
2017-01-25 14:45:38 +0100

    Added activation and deactivation messages

--- src/AspirantiPluginner/ServerIP/Main.php
+++ src/AspirantiPluginner/ServerIP/Main.php
@@ -14,7 +14,21 @@
 namespace AspirantiPluginner\ServerIP;
 
 use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
+use pocketmine\Server;
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginManager;
+use pocketmine\plugin\Plugin;
+use pocketmine\utils\config;
+use pocketmine\level\Level;
 
 class Main extends PluginBase {
-    //test
+
+    public function onEnable(){
+        $this->getLogger()->info(TextFormat::GREEN ."ServerIP Enabled!");
+    }
+
+    public function onDisable(){
+        $this->getLogger()->info(TextFormat::RED ."ServerIP Disabled");
+    }
 }
