fb7237ab708ec57daba10afaad649c012fa8a7a3
TheNewHEROBRINEX <luca.biazzetti@gmail.com>
2017-01-25 21:12:09 +0100

    Il plugin logga l'ip del server in onEnable()

--- src/AspirantiPluginner/ServerIP/Main.php
+++ src/AspirantiPluginner/ServerIP/Main.php
@@ -14,12 +14,15 @@
 namespace AspirantiPluginner\ServerIP;
 
 use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
 
 class Main extends PluginBase {
 
+    /** @var string $ip Property dove metteremo l'ip da usare in seguito nel comando */
+    public $ip;
+
     public function onEnable() {
-        $this->getServer()->getLogger(OnEnable $ip = file_get_contents("http://ip-api.com/line/?fields=query"));
-        
-        // :D
- }
+        //posso contemporaneamente salavre l'ip nella property e scriverlo sulla console
+        $this->getServer()->getLogger()->info("L'IP del server è " . TextFormat::AQUA . $this->ip = trim(file_get_contents("http://ip-api.com/line/?fields=query")));
+    }
 }
